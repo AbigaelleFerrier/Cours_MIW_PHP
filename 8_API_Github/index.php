df4a7a1d966ae81765dc382f8b12886999ff5c2c
cirill <cirill@asheart.fr>
2019-12-09 16:28:52 +0100

    /*/

--- 8_API_Github/index.php
+++ 8_API_Github/index.php
@@ -1,28 +1,16 @@
 <?php 
 
-$url = 'http://api.github.com/users/FerrierCirill';
-
+$url = 'https://api.github.com/repos/FerrierCirill/directory/contents/peoples.json';
 $ch = curl_init();
 
-var_dump($ch);
-
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array ('Content-Type: application/json'));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
-curl_setopt($ch, CURLOPT_USERAGENT,'User-Agent: Awesome-Octocat-App');
-curl_setopt($ch, CURLOPT_XOAUTH2_BEARER, '011e6f55f884f2253323f5baa29d478347f0d848');
-// var_dump(curl_getinfo ($ch));
+curl_setopt($ch, CURLOPT_URL            , $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER , 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER     , array('Content-Type: application/json'));
+curl_setopt($ch, CURLOPT_USERAGENT      , 'User-Agent: Awesome-Octocat-App');
+curl_setopt($ch, CURLOPT_XOAUTH2_BEARER , '011e6f55f884f2253323f5baa29d478347f0d848');
 
 $cont =  curl_exec($ch);
-
-// var_dump($cont);
-
 curl_close($ch);
 
-var_dump(json_decode($cont));
-
-
-curl_close($ch);
-
-
+$monJson = json_decode($cont); 
+echo(base64_decode($monJson->{'content'}));
