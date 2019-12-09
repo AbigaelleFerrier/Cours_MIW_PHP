e26dcd6b8da39e062113ec6bb4839b69e9dca34f
cirill <cirill@asheart.fr>
2019-12-09 16:32:45 +0100

    /*-*/

--- 8_API_Github/index.php
+++ 8_API_Github/index.php
@@ -7,7 +7,7 @@ curl_setopt($ch, CURLOPT_URL            , $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER , 1);
 curl_setopt($ch, CURLOPT_HTTPHEADER     , array('Content-Type: application/json'));
 curl_setopt($ch, CURLOPT_USERAGENT      , 'User-Agent: Awesome-Octocat-App');
-curl_setopt($ch, CURLOPT_XOAUTH2_BEARER , '011e6f55f884f2253323f5baa29d478347f0d848');
+curl_setopt($ch, CURLOPT_XOAUTH2_BEARER , 'xxx');
 
 $cont =  curl_exec($ch);
 curl_close($ch);
